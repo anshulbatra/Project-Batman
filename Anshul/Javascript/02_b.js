45f03d15f964b11e7cb34094d5ecd57ee0dd6591
Anshul Batra <31509682+anshulbatra@users.noreply.github.com>
2019-03-18 23:17:31 +0530

    External js file for 02_a.html

--- Anshul/Javascript/02_b.js
+++ Anshul/Javascript/02_b.js
@@ -1,5 +1,5 @@
 function myFunction() 
 {
-   //To access an HTML element, JavaScript can use the document.getElementById(id) method
+   //To access an HTML element, JavaScript can use the document.getElementById(id) method.
    document.getElementById("para").innerHTML = "Paragraph changed.";
 }
